ad627f9b6cf1dbd171614763910feb92c776f53e
Jenifer De Figueiredo <jeny.defigueiredo@gmail.com>
2016-01-12 19:11:49 +0000

    Document changes in comment

--- train.py
+++ train.py
@@ -184,4 +184,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:51983
+# Comment to update:519838

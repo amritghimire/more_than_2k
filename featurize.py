e9af8b3ec4ea66cad12653a4933a4795c4a37947
Laurens Duijvesteijn <laurens@iterative.ai>
2019-02-16 00:12:09 +0000

    Fix a typo

--- featurize.py
+++ featurize.py
@@ -66,4 +66,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:481004659449
+# Comment to update:4810046594493

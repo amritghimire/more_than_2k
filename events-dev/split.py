12bb55fefefefabb8a2346e3ff65ffb475f3026f
Laurens Duijvesteijn <laurens@iterative.ai>
2022-04-08 21:19:48 +0000

    Fix split.py

--- events-dev/split.py
+++ events-dev/split.py
@@ -81,4 +81,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:018323890843484162013
+# Comment to update:0183238908434841620130

01dcaa4fc6c9067385c7e2a8259ed798e5f8ad35
Laurens Duijvesteijn <laurens@iterative.ai>
2022-05-13 14:38:30 +0000

    Update comment

--- featurize.py
+++ featurize.py
@@ -66,4 +66,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:48100465944939200196
+# Comment to update:481004659449392001962

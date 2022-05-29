b9ab3eb473bf4df009c713d8b340c6c5b27e6799
Laurens Duijvesteijn <laurens@iterative.ai>
2022-05-29 06:19:17 +0000

    Update split

--- events-dev/split.py
+++ events-dev/split.py
@@ -81,4 +81,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:0183238908434841620130
+# Comment to update:01832389084348416201301

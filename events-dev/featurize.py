b1916cb54844f0708b6230acb917b76d7532c40b
Laurens Duijvesteijn <laurens@iterative.ai>
2022-06-10 02:08:34 +0000

    Fix featurize

--- events-dev/featurize.py
+++ events-dev/featurize.py
@@ -69,4 +69,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:05472457698356688
+# Comment to update:054724576983566881

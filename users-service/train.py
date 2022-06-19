6c43797696e3e45513ff88d427d1537abc42cfa2
Jenifer De Figueiredo <jeny.defigueiredo@gmail.com>
2022-06-19 17:10:46 +0000

    Update comment

--- users-service/train.py
+++ users-service/train.py
@@ -124,4 +124,4 @@ with open(sys.argv[1]) as fd, open(sys.argv[2], "w") as model:
     data["work"].append(VERSION)
     json.dump(data, model)
 
-# Comment to update:272995487460531067
+# Comment to update:2729954874605310677

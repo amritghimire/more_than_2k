4f92398288ff372e1bab73de3232241b86d70a91
Jenifer De Figueiredo <jeny.defigueiredo@gmail.com>
2022-06-19 03:30:13 +0000

    Add a note

--- questions-set/featurize.py
+++ questions-set/featurize.py
@@ -69,4 +69,4 @@ with open(sys.argv[2], "w") as feat:
     data_size = os.stat(sys.argv[1]).st_size
     json.dump({"params": params_values, "work": [math.log(data_size + 1), VERSION]}, feat)
 
-# Comment to update:1916277093318728
+# Comment to update:19162770933187287

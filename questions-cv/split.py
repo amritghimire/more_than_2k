a784ea5dd889ebeb453371b523e76510c51c8fe2
Jenifer De Figueiredo <jeny.defigueiredo@gmail.com>
2022-09-02 08:11:46 +0000

    Fix a typo

--- questions-cv/split.py
+++ questions-cv/split.py
@@ -72,4 +72,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:1851235804582
+# Comment to update:18512358045827

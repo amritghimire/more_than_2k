8bd9c541e8ed55739763da237d2362f7daab5ccd
Laurens Duijvesteijn <laurens@iterative.ai>
2023-02-02 21:55:04 +0000

    Fix a typo in comment

--- users-service/split.py
+++ users-service/split.py
@@ -66,4 +66,4 @@ with open(sys.argv[1]) as raw, open(sys.argv[2], "w") as train, open(sys.argv[3]
         else:
             train.write(line)
 
-# Comment to update:920036286388626
+# Comment to update:9200362863886260
